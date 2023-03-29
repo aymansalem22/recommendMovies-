653fa433d07a345a9092b051793b1ed18400c6b2
aymansalem22 <s.aymansalem@gmail.com>
2023-03-29 03:54:51 +0200

    update Unit Testing Using Spring Boot

--- src/test/java/io/datajek/spring/basics/movierecommendersystem/MovieRecommenderSystemApplicationTests.java
+++ src/test/java/io/datajek/spring/basics/movierecommendersystem/MovieRecommenderSystemApplicationTests.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
 import io.datajek.spring.basics.movierecommendersystem.lesson4.*;
 
 @SpringBootTest
