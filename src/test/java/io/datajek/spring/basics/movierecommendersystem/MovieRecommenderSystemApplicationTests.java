4f60374f1fbde38655d4b1e2640f4d16b6622b83
aymansalem22 <s.aymansalem@gmail.com>
2023-03-29 03:48:27 +0200

    Unit Testing Using Spring Boot

--- src/test/java/io/datajek/spring/basics/movierecommendersystem/MovieRecommenderSystemApplicationTests.java
+++ src/test/java/io/datajek/spring/basics/movierecommendersystem/MovieRecommenderSystemApplicationTests.java
@@ -1,13 +1,22 @@
 package io.datajek.spring.basics.movierecommendersystem;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import io.datajek.spring.basics.movierecommendersystem.lesson4.*;
 
 @SpringBootTest
 class MovieRecommenderSystemApplicationTests {
 
+	@Autowired
+	private RecommenderImplementation recommenderImpl;
+
 	@Test
-	void contextLoads() {
+	void testRecommendMovies() {
+		assertArrayEquals(new String[] { "Finding Nemo", "Ice Age", "Toy Story" },
+				recommenderImpl.recommendMovies("Finding Dory"));
 	}
 
 }
